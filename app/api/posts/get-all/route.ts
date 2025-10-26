fc693ef156101897c089166861bbbd0ff595216a
ARIJIT BARIK <arijitbarik655@gmail.com>
2025-10-26 09:15:53 +0530

    prisma setup is done, api started

---
+++ app/api/posts/get-all/route.ts
@@ -0,0 +1,10 @@
+import db from "@/lib/prisma";
+
+export async function GET() {
+  const data = await db.post.findMany();
+  return Response.json({
+    status: "200",
+    data,
+    message: "this is api route for get post",
+  });
+}
